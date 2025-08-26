8454d1ae8e61cff8374b90737cbb4c32e9563019
duststars <i@exploro.one>
2025-08-27 05:51:45 +0800

    build crds into current directory

--- gen-crds.sh
+++ gen-crds.sh
@@ -3,4 +3,4 @@
 scriptPath=$(realpath $0)
 scriptDir=$(dirname $scriptPath)
 
-cd "$scriptDir" && controller-gen crd paths=./pkg/apis/networking/v1alpha1 output:stdout output:crd:dir=../crds
+cd "$scriptDir" && controller-gen crd paths=./pkg/apis/networking/v1alpha1 output:stdout output:crd:dir=./crds
